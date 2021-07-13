c2a9d1de86f9e7b9371f601e902fd2b5d0928e36
Justin <pehyuxiang@gmail.com>
2021-07-13 12:25:19 +0800

    Add curve fitting code
    Lookup old source code on monte-carlo 2D least-squares fitting

--- python/graphing.py
+++ python/graphing.py
@@ -15,3 +15,23 @@ matplotlib.rcParams["toolbar"] = "None" # hide nav toolbars
 import matplotlib.pyplot as plt
 plt.rcParams.update({'axes.titlesize': 'x-large',
                      'axes.labelsize': 'x-large'})
+
+
+### Curve fitting ###
+
+from scipy.optimize import curve_fit
+import numpy as np
+
+def _cool_exp(t, T1, T0, tau):
+    return T0 + (T1-T0)*np.exp(-t/tau)
+
+def test():
+    with open("20210712_cool_data.dat") as f:
+        data = f.read()
+        data = [[float(d) for d in row.split(" ")] for row in data.split("\n") if row != ""]
+
+    xs, ys = list(zip(*data))
+    popt, pcov = curve_fit(_cool_exp, xs, ys)
+    fit = list(zip(popt, np.sqrt(np.diag(pcov))))
+    for param, param_err in fit:
+        print(param, "+/-", param_err)
